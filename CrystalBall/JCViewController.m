396aa80012a1e6a4183e3e3e4449c24d16f2d719
Justin Cano <jcano001@ucr.edu>
2014-02-21 01:09:40 -0800

    added sound

--- CrystalBall/JCViewController.m
+++ CrystalBall/JCViewController.m
@@ -8,16 +8,24 @@
 
 #import "JCViewController.h"
 #import "JCCrystalBall.h"
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface JCViewController ()
 
 @end
 
-@implementation JCViewController
+@implementation JCViewController {
+    SystemSoundID soundEffect;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSString *soundPath = [[NSBundle mainBundle] pathForResource:@"crystal_ball" ofType:@"mp3"];
+    NSURL *soundURL = [NSURL fileURLWithPath:soundPath];
+    AudioServicesCreateSystemSoundID(CFBridgingRetain(soundURL), &soundEffect);
+    
     self.crystalBall = [[JCCrystalBall alloc] init];
     self.backgroundImageView.animationImages = [[NSArray alloc] initWithObjects:
                                                 [UIImage imageNamed:@"CB00001"],
@@ -96,12 +104,19 @@
 -(void) makePrediction {
     [self.backgroundImageView startAnimating];
     self.predictionLabel.text = [self.crystalBall randomPrediction];
+    
+    AudioServicesPlaySystemSound(soundEffect);
+    
+    [UIView animateWithDuration:6.0 animations:^{
+        self.predictionLabel.alpha = 1.0f;
+    }];
 }
 
 #pragma mark - Motion Events
 
 -(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
     self.predictionLabel.text = nil;
+    self.predictionLabel.alpha = 0.0f;
 }
 
 -(void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
@@ -118,6 +133,7 @@
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     self.predictionLabel.text = nil;
+    self.predictionLabel.alpha = 0.0f;
 }
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
